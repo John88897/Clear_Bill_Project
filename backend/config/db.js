0710ea79501eb3e46a99c96428fb8b203c82591e
John88897 <sreyka1m13579@gmail.com>
2026-07-12 09:23:40 +0700

    add ssl

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,26 @@
 const { Sequelize } = require("sequelize");
 const dotenv = require('dotenv');
 dotenv.config();
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
-    // process.env.DB_PORT,
     {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    },
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: "mysql",
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+        },
+        logging: false
+    }
 );
-// sequelize.sync({force:true})
-// sequelize.sync({alter: true})
+
 console.log("DB_NAME =", process.env.DB_NAME);
 console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_HOST =", process.env.DB_HOST);
+
 module.exports = sequelize;
